d1c67175898338c5c1d580d8ff2feae573353c9d
Nicholas Rutherford <nicholasrutherford121@gmail.com>
2020-11-07 12:23:13 -0600

    remove duplicate/unneeded method names

--- code/app/src/main/java/com/nicholasrutherford/chal/helpers/Typeface.kt
+++ code/app/src/main/java/com/nicholasrutherford/chal/helpers/Typeface.kt
@@ -6,11 +6,6 @@ import android.widget.TextView
 
 class Typeface {
 
-    fun setHeaderTypefaceBold(textView: TextView, context: Context, path: String) {
-        val typefaceHeaderBold = Typeface.createFromAsset(context.assets, "font/Roboto-Bold.ttf")
-        textView.typeface = typefaceHeaderBold
-    }
-
     fun setTypefaceForHeaderBold(textView: TextView, context: Context) {
         val typefaceHeaderBold = Typeface.createFromAsset(context.assets, "font/Roboto-Bold.ttf")
         textView.typeface = typefaceHeaderBold
@@ -19,11 +14,6 @@ class Typeface {
     fun setTypefaceForHeaderRegular(textView: TextView, context: Context) {
         val typefaceHeaderRegular = Typeface.createFromAsset(context.assets, "font/Roboto-Regular.ttf")
         textView.typeface = typefaceHeaderRegular
-    }
-
-    fun setTypefaceForBoldSubHeader(textView: TextView, context: Context, path: String) {
-        val typefaceSubHeaderBold = Typeface.createFromAsset(context.assets, "font/Roboto-Regular.ttf")
-        textView.typeface = typefaceSubHeaderBold
     }
 
     fun setTypefaceForSubHeaderBold(textView: TextView, context: Context) {
@@ -36,24 +26,9 @@ class Typeface {
         textView.typeface = typefaceSubHeaderRegular
     }
 
-    fun setTypefaceForRegularSubHeader(textView: TextView, context: Context, path: String) {
-        val typefaceSubHeaderRegular = Typeface.createFromAsset(context.assets, "font/Roboto-Regular.ttf")
-        textView.typeface = typefaceSubHeaderRegular
-    }
-
-    fun setTypefaceForBoldBody(textView: TextView, context: Context, path: String) {
-        val typefaceBodyBold = Typeface.createFromAsset(context.assets, "font/OpenSans-Bold.ttf")
-        textView.typeface = typefaceBodyBold
-    }
-
     fun setTypefaceForBodyBold(textView: TextView, context: Context) {
         val typefaceBodyBold = Typeface.createFromAsset(context.assets, "font/OpenSans-Bold.ttf")
         textView.typeface = typefaceBodyBold
-    }
-
-    fun setTypefaceForItalicBody(textView: TextView, context: Context, path: String) {
-        val typefaceBodyItalic = Typeface.createFromAsset(context.assets, "font/OpenSans-Italic.ttf")
-        textView.typeface = typefaceBodyItalic
     }
 
     fun setTypefaceForBodyItalic(textView: TextView, context: Context) {
@@ -61,12 +36,12 @@ class Typeface {
         textView.typeface = typefaceBodyItalic
     }
 
-    fun setTypefaceForLightBody(textView: TextView, context: Context, path: String) {
+    fun setTypefaceForLightBody(textView: TextView, context: Context) {
         val typefaceBodyLight = Typeface.createFromAsset(context.assets, "font/OpenSans-Light.ttf")
         textView.typeface = typefaceBodyLight
     }
 
-    fun setTypefaceForRegularBody(textView: TextView, context: Context, path: String) {
+    fun setTypefaceForRegularBody(textView: TextView, context: Context) {
         val typefaceBody = Typeface.createFromAsset(context.assets, "font/OpenSans-Regular.ttf")
         textView.typeface = typefaceBody
     }
