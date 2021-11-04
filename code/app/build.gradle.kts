5e5d86188ea339bd52b7a56054fb56ea515fbc55
Nicholas Rutherford <nrutherf@BRK-204081.attlocal.net>
2021-11-03 21:56:43 -0500

    remove shit

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -1,5 +1,3 @@
-apply(plugin = Dependencies.Plugin.GoogleServices)
-
 plugins {
     id(Dependencies.Plugin.Application)
     kotlin(Dependencies.Plugin.Android)
@@ -14,8 +12,6 @@ android {
     defaultConfig {
         minSdk = Dependencies.Android.MinSdkVersion
     }
-
-    buildFeatures.viewBinding = true
 
 
     defaultConfig {
@@ -48,66 +44,11 @@ android {
 }
 
 dependencies {
-    api(project(path = ":account:validation"))
-    api(project(path = ":firebase:auth"))
-    api(project(path = ":helper:fragment"))
     api(project(path = ":main:activity"))
-    api(project(path = ":main:resources"))
-    api(project(path = ":network"))
-    api(project(path = ":ui:base-fragment"))
-    api(project(path = ":ui:base-vm"))
-    api(project(path = ":ui:typefaces"))
 
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation(Dependencies.Libs.UI.AcProgress)
-    implementation(Dependencies.Libs.Android.AppCompat)
-
-    implementation(Dependencies.Libs.Debug.TestFairy)
     implementation(Dependencies.Libs.Debug.Timber)
-    implementation(Dependencies.Libs.Kotlin.Coroutines)
-    implementation(Dependencies.Libs.UI.SwipeRefreshLayout)
-
-    implementation(Dependencies.Libs.UI.ConstraintLayout)
-    implementation(Dependencies.Libs.UI.CircleImageView)
-
-    implementation(Dependencies.Libs.Kotlin.Jdk)
-    implementation(Dependencies.Libs.Android.Ktx)
-    implementation(Dependencies.Libs.UI.Material)
-
-    implementation(Dependencies.Libs.LifeCycle.LiveData)
-    implementation(Dependencies.Libs.LifeCycle.RunTime)
-    implementation(Dependencies.Libs.LifeCycle.ViewModel)
-
-    implementation(Dependencies.Libs.Image.Glide)
-    annotationProcessor(Dependencies.Libs.Image.GlideCompiler)
-    implementation(Dependencies.Libs.Image.Picasso)
-
-    implementation(Dependencies.Libs.Firebase.Auth)
-    implementation(Dependencies.Libs.Firebase.Analytics)
-    implementation(Dependencies.Libs.Firebase.Database)
-    implementation(Dependencies.Libs.Firebase.Storage)
-
-    androidTestImplementation(Dependencies.LibsTest.Espresso.Core)
-    androidTestImplementation(Dependencies.LibsTest.Junit.JunitTestImplementation)
-    testImplementation(Dependencies.LibsTest.Junit.Native)
-
-    implementation(Dependencies.Libs.Gson.Native)
-
-    kapt(Dependencies.Libs.Room.Compiler)
-    implementation(Dependencies.Libs.Room.Ktx)
-    implementation(Dependencies.Libs.Room.Runtime)
-
-    implementation(Dependencies.Libs.Dagger.Android)
-    implementation(Dependencies.Libs.Dagger.Native)
-    implementation(Dependencies.Libs.Dagger.Support)
-
-    // Navigation
-    implementation(Dependencies.Libs.Navigation.NavigationFragment)
-    implementation(Dependencies.Libs.Navigation.NavigationUi)
-
-    kapt(Dependencies.Libs.Dagger.Compiler)
-    kapt(Dependencies.Libs.Dagger.Processor)
 
     implementation(Dependencies.Libs.Hilt.DaggerHiltAndroid)
     kapt(Dependencies.Libs.Hilt.DaggerHiltAndroidCompiler)
